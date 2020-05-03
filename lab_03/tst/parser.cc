5d1a3701b3c31e44c26e84a0616a58fb56cc89a1
Andrey Kamakin <a.kamakin@icloud.com>
2020-05-03 22:13:04 +0300

    added tests

--- lab_03/tst/parser.cc
+++ lab_03/tst/parser.cc
@@ -3,6 +3,86 @@
 
 #include "parser.h"
 
+TEST(ParserTest, Test1)
+{
+    auto &&[tree, str] = parser::accept("{a=const}");
+
+    EXPECT_TRUE(tree);
+    EXPECT_TRUE(str.empty());
+}
+
+TEST(ParserTest, Test2)
+{
+    auto &&[tree, str] = parser::accept("{a=const;}");
+
+    EXPECT_TRUE(!tree);
+    EXPECT_TRUE(!str.empty());
+}
+
+TEST(ParserTest, Test3)
+{
+    auto &&[tree, str] = parser::accept("{a=const;a=b*a}");
+
+    EXPECT_TRUE(tree);
+    EXPECT_TRUE(str.empty());
+}
+
+TEST(ParserTest, Test4)
+{
+    auto &&[tree, str] = parser::accept("{a=const;{a=(b)*a}}");
+
+    EXPECT_TRUE(tree);
+    EXPECT_TRUE(str.empty());
+}
+
+TEST(ParserTest, Test5)
+{
+    auto &&[tree, str] = parser::accept("{a=const;{a=-a}}");
+
+    EXPECT_TRUE(tree);
+    EXPECT_TRUE(str.empty());
+}
+
+TEST(ParserTest, Test6)
+{
+    auto &&[tree, str] = parser::accept("{a=const<>a-a}");
+
+    EXPECT_TRUE(tree);
+    EXPECT_TRUE(str.empty());
+}
+
+TEST(ParserTest, Test7)
+{
+    auto &&[tree, str] = parser::accept("{a=const<>a;{b=notconst}}");
+
+    EXPECT_TRUE(tree);
+    EXPECT_TRUE(str.empty());
+}
+
+TEST(ParserTest, Test8)
+{
+    auto &&[tree, str] = parser::accept("{a = a div b mod c}");
+
+    EXPECT_TRUE(tree);
+    EXPECT_TRUE(str.empty());
+}
+
+TEST(ParserTest, Test9)
+{
+    auto &&[tree, str] = parser::accept("{a = a div b mod c; {a = +b <= const}}");
+
+    EXPECT_TRUE(tree);
+    EXPECT_TRUE(str.empty());
+}
+
+TEST(ParserTest, Test10)
+{
+    auto &&[tree, str] = parser::accept("{}");
+
+    EXPECT_TRUE(!tree);
+    EXPECT_TRUE(!str.empty());
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
